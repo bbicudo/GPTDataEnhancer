73259860a9582cbbf40c096eade965fa488b2ecc
Bruno Branco Bicudo <brunobranco4@hotmail.com>
2025-01-14 18:45:44 -0300

    Remove 'demo' from package name in the project structure. (#2)
    Co-authored-by: Bruno Branco Bicudo <bruno.bicudo@connectbr.dev>

---
+++ src/test/java/com/gpt/data/enhancer/GptDataEnhancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpt.data.enhancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptDataEnhancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
